644fb3e10fb65b21eea7a1edfcf0b145b0553ab7
Padawan2612 <mjoeln2612@hotmail.com>
2022-09-12 19:50:02 -0500

    Page-information

--- src/app/pages/common/common-routing.module.ts
+++ src/app/pages/common/common-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { PageInformationComponent } from './page-information/page-information.component';
 
 const routes: Routes = [
   { path: 'not-found', component: NotFoundComponent },
+  { path: 'page', component: PageInformationComponent},
 ];
 
 @NgModule({
--- src/app/pages/common/common.module.ts
+++ src/app/pages/common/common.module.ts
@@ -4,9 +4,10 @@ import { CommonModule as NgCommonModule } from '@angular/common';
 import { CommonRoutingModule } from './common-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { PageInformationComponent } from './page-information/page-information.component';
 
 @NgModule({
-  declarations: [NotFoundComponent],
+  declarations: [NotFoundComponent, PageInformationComponent],
   imports: [NgCommonModule, CommonRoutingModule, SharedModule],
 })
 export class CommonModule {}
