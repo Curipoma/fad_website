543013187271fdbfdd4931a05d0e7afdccaa4530
Curipoma <at.curipoma@yavirac.edu.ec>
2022-09-13 00:28:02 -0500

    add page - file not found

--- src/app/pages/common/not-found/not-found.component.ts
+++ src/app/pages/common/not-found/not-found.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent implements OnInit {
-
-  constructor() { }
+  statusTitle: string | undefined = '';
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.statusTitle = this.activatedRoute.snapshot.routeConfig?.['path'];
   }
-
 }
